b77b85774b34fde297e6ec23868f916832b76288
Peter Waller <p@pwaller.net>
2015-03-05 20:12:47 +0000

    Split deadliner.go

--- main.go
+++ main.go
@@ -11,38 +11,6 @@ import (
 	"github.com/scraperwiki/s4log/poller"
 )
 
-type Deadliner struct {
-	mu     sync.Mutex
-	period time.Duration
-	next   time.Time
-}
-
-func NewDeadliner(period time.Duration) *Deadliner {
-	d := &Deadliner{period: period, next: time.Now()}
-	d.Met()
-	return d
-}
-
-func (d *Deadliner) Deadline() time.Time {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-	return d.next
-}
-
-// When a deadline is met, a new deadline is issued
-func (d *Deadliner) Met() {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-	d.next = d.next.Add(d.period)
-}
-
-// Until returns time until the next deadline. May be negative if unmet.
-func (d *Deadliner) Until() time.Duration {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-	return -time.Since(d.next)
-}
-
 func Stream(args []string) (in io.Reader, wait func()) {
 	cmd := exec.Command(args[0], args[1:]...)
 	stdout, err := cmd.StdoutPipe()
