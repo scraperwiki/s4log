5cda057902e072b19f67a5b962a1365ca486d80e
Peter Waller <p@pwaller.net>
2015-03-14 13:29:52 +0000

    Move Commit() to commit.go

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"io"
 	"log"
 	"os"
@@ -11,74 +9,6 @@ import (
 
 	"github.com/scraperwiki/s4log/poller"
 )
-
-func WriteBuf(name string, data []byte) error {
-	fd, err := os.Create(name)
-	if err != nil {
-		return fmt.Errorf("failed to os.Create: %v", err)
-	}
-	defer fd.Close()
-
-	nw, err := fd.Write(data)
-	if err != nil || nw != len(data) {
-		log.Printf("Error or short write: (%v leftover): %v", len(data)-nw, err)
-		if err != nil {
-			err = io.ErrShortWrite
-		}
-		return err
-	}
-	return err
-}
-
-// Commits `buf` to permanent storage, up to the final newline.
-// If there is data following the final newline, it is moved to the beginning.
-func Commit(wg *sync.WaitGroup, hostname string, buf []byte) int {
-	if len(buf) == 0 {
-		// Nothing to do
-		return 0
-	}
-	idx := bytes.LastIndex(buf, []byte("\n"))
-
-	var p []byte
-	if idx == -1 {
-		// No newline, take everything
-		p = buf
-	} else {
-		// take up to the last newline, including the newline
-		p = buf[:idx+1]
-	}
-
-	// Copy the data to a fresh buffer
-	newbuf := make([]byte, len(p))
-	copy(newbuf, p)
-
-	// Commence an asynchronous copy of the buffer to permanent storage.
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-
-		timestamp := time.Now().Format(time.RFC3339Nano)
-		name := fmt.Sprintf("logs/%s-%s.txt", hostname, timestamp)
-		log.Printf("Committing %d bytes to %q", len(newbuf), name)
-
-		err := WriteBuf(name, newbuf)
-		if err != nil {
-			log.Printf("Error committing %q: %v", name, err)
-			return
-		}
-
-		nMu.Lock()
-		defer nMu.Unlock()
-		n += len(newbuf)
-	}()
-
-	// Move trailing data to beginning of `buf` and truncate `buf`
-	copy(buf, buf[len(p):])
-	leftOver := len(buf) - len(p)
-	buf = buf[:leftOver]
-
-	return leftOver
-}
 
 var (
 	nMu sync.Mutex
