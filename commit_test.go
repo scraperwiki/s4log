343bb2ea22ebd05a9b4dea8b7c9954bd5e623809
Peter Waller <p@pwaller.net>
2015-03-14 15:35:36 +0000

    Add NewlineCommitter test

---
+++ commit_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestNewlineCommitter(t *testing.T) {
+	var committed string
+	nlc := NewlineCommitter{CommitterFunc(func(buf []byte) int {
+		committed = string(buf)
+		return 0
+	})}
+
+	cases := []struct {
+		buf, committed, remaining string
+	}{
+		{"first\nsecond", "first\n", "second"},
+		{"first\n", "first\n", ""},
+		{"\nsecond", "\n", "second"},
+		{"noNewLine", "noNewLine", ""},
+	}
+
+	for _, c := range cases {
+		buf := []byte(c.buf)
+		remain := nlc.Commit(buf)
+		after := string(buf[:remain])
+
+		t.Logf("buf=%q, commit=%q, after=%q", c.buf, committed, after)
+
+		if after != c.remaining {
+			t.Errorf("expected %q after, got %v", c.remaining, after)
+		}
+
+		if committed != c.committed {
+			t.Errorf("expected %q committed, got %q", c.committed, committed)
+		}
+	}
+}
